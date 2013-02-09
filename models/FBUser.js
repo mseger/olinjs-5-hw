88e6c2589b7a4f1167e65f7d0ac24ab91c6e98a8
Margaret-Ann Seger <margaret-ann@Margaret-Anns-MacBook-Air.local>
2013-02-09 17:27:52 -0500

    Fixed new vs. returning users

--- models/FBUser.js
+++ models/FBUser.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose'), Schema = mongoose.Schema
 var FBUserSchema = new Schema({
 	name: String, 
 	profPicURL: String, 
-	profileBackground: String
+	profileBackground: String,
+	quotes: [String]
 });
 
 var FBUser = mongoose.model('FBUser', FBUserSchema);
--- routes/FBUser.js
+++ routes/FBUser.js
@@ -3,22 +3,22 @@ var FBUser = require('../models/FBUser')
 // login a new user, start a new session
 exports.login = function (req, res) {
   req.facebook.api('/me', function(err, data){
+  	console.log("USER INFO IS: ", data.quotes);
   	req.facebook.api('/me/picture?redirect=false&type=large', function(err, picData){
-  		if(FBUser.findOne({name: data.name})){
-  			var currUser = FBUser.findOne({name: data.name}, function (err, user){
-  				console.log("Found user: ", user); 
+  		var existentUser = FBUser.findOne({name: data.name}, function (err, user){
+  			if(user){
   				req.session.user = user;
   				res.redirect('/homepage');
-  			});
-  		}else{
-  			var loggedInUser = new FBUser({name: data.name, profPicURL: picData.data.url, profileBackground: "white"});
-  				loggedInUser.save(function (err){
-		  			if(err)
-		  				console.log("Unable to save new user.");
-		  		 	req.session.user = loggedInUser; 
-		  			res.redirect('/homepage');
-  				});
-  			}
+	  		}else{
+	  			var loggedInUser = new FBUser({name: data.name, profPicURL: picData.data.url, profileBackground: "white", quotes: data.quotes});
+	  				loggedInUser.save(function (err){
+			  			if(err)
+			  				console.log("Unable to save new user.");
+			  		 	req.session.user = loggedInUser; 
+			  			res.redirect('/homepage');
+	  				});
+	  			}
+	  		});
   		});
   });
 };
